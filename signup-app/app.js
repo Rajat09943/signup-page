c11d5616d6c2ee3239cee588161319ffe2dc34fc
Rajat09943 <rajatsapehia123@gmail.com>
2024-09-16 11:28:10 +0530

    first commit

---
+++ signup-app/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname)));
+
+app.post('/signup', (req, res) => {
+    const { username, email, password } = req.body;
+
+    const userData = `Username: ${username}, Email: ${email}, Password: ${password}\n`;
+
+    fs.appendFile('userdata.txt', userData, (err) => {
+        if (err) {
+            console.error('Error saving user data:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        console.log('User data saved!');
+        res.send(`Thank you for signing up, ${username}!`);
+    });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
